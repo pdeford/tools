704dd9603f845f08e7c739c5aa1c684a9d37fde8
pdeford <pdeford1@jhu.edu>
2015-11-27 13:45:23 -0500

    Simple progress bar for tracking progress

---
+++ progress_bar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+class progress_bar(object):
+	"""docstring for progress_bar"""
+	def __init__(self, length=50):
+		self.length = length
+	def updateProgress(self, progress, total):
+		"""Write the progress to screen as a bar and a percent."""
+		l = int(self.length)
+		percent = progress*100.0/total
+		bars = int(percent//(100.0/l))
+		bar = "[" + "="*bars + " "*(l-bars) + "] %0.2f%%" % percent + "\r"
+		sys.stdout.write(bar)
+		sys.stdout.flush()
+		sys.stdout.flush()
+		if percent >= 100.0:
+			print bar[:-1]
